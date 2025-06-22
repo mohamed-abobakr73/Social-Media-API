10eafa280d92b44322f69dad0f89b8bb0b581c31
= <mohamedabobakr04@gmail.com>
2025-06-22 15:07:16 +0200

    added index file for the services folder for easier imports

---
+++ src/services/index.ts
@@ -0,0 +1,19 @@
+import chatsServices from "./chatsServices";
+import usersServices from "./usersServices";
+import reportsServices from "./reportsServices";
+import pagesServices from "./pagesServices";
+import postsServices from "./postsServices";
+import searchServices from "./searchServices";
+import groupsServices from "./groupsServices";
+import notificationsServices from "./notificationsServices";
+
+export {
+  chatsServices,
+  usersServices,
+  reportsServices,
+  pagesServices,
+  postsServices,
+  searchServices,
+  groupsServices,
+  notificationsServices,
+};
